02b967729e3af9e51ffc5547dfa96f233bfe7e73
Ivan Mikushin <imikushin@users.noreply.github.com>
2026-04-08 11:34:17 -0700

    chore(client): add support for protocol v13

--- charms-client/src/lib.rs
+++ charms-client/src/lib.rs
@@ -42,6 +42,8 @@ pub const V9_SPELL_VK: &str = "0x00713f077ec2bd68157512835dc678053565a889935ecd5
 pub const V10_SPELL_VK: &str = "0x00ccf030317cae019a4cd3c8557b2c5b522050e7e562e3adf287cd5ad596511f";
 /// Verification key for version `11` of the protocol implemented by `charms-proof-wrapper` binary.
 pub const V11_SPELL_VK: &str = "0x00d41d49f54303acee4e7d064a31e0c9bd2e1bbdb60f39170a1461c71015c308";
+/// Verification key for version `12` of the protocol implemented by `charms-proof-wrapper` binary.
+pub const V12_SPELL_VK: &str = "0x00cd44537c67da0dc50b88e794deed43c4507a862070ed83c99941789811a6a0";
 
 /// Version `0` of the protocol.
 pub const V0: u32 = 0;
@@ -69,9 +71,11 @@ pub const V10: u32 = 10;
 pub const V11: u32 = 11;
 /// Version `12` of the protocol.
 pub const V12: u32 = 12;
+/// Version `13` of the protocol.
+pub const V13: u32 = 13;
 
 /// Current version of the protocol.
-pub const CURRENT_VERSION: u32 = V12;
+pub const CURRENT_VERSION: u32 = V13;
 
 /// Source of a beamed input: the UTXO that beamed the charms, with an optional nonce.
 ///
